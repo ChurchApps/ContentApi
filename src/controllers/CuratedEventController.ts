71804dff2bdfc6ce471778b8eb4568a337bdecd0
Himali Malvawala <86650000+Himali-Malvawala@users.noreply.github.com>
2023-05-29 09:47:25 +0530

    feat: allow to add groups to curatedEvents (#15)
    * feat: allow to add groups to curatedEvents
    
    * improve
    
    * remove DateTimeHelper import
    
    * update save function
    
    * update get function
    
    ---------
    
    Co-authored-by: Pranav Malvawala <pranavmalvawala@gmail.com>

--- src/controllers/CuratedEventController.ts
+++ src/controllers/CuratedEventController.ts
@@ -1,11 +1,32 @@
 import { controller, httpPost, httpGet, interfaces, requestParam, httpDelete } from "inversify-express-utils";
 import express from "express";
 import { ContentBaseController } from "./ContentBaseController"
-import { CuratedEvent } from "../models"
+import { CuratedEvent, Event } from "../models"
 import { Permissions } from "../helpers";
 
 @controller("/curatedEvents")
 export class CuratedEventController extends ContentBaseController {
+
+  @httpGet("/calendar/:curatedCalendarId")
+  public async getForCuratedCalendar(@requestParam("curatedCalendarId") curatedCalendarId: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      const curatedEvents: CuratedEvent[] = await this.repositories.curatedEvent.loadByCuratedCalendarId(au.churchId, curatedCalendarId);
+      if (req.query?.with === "eventData") {
+        const promises: Promise<CuratedEvent>[] = [];
+        curatedEvents?.forEach(c => {
+          promises.push(this.repositories.event.load(au.churchId, c.eventId).then((eventData: Event) => {
+            c.eventData = eventData;
+            return c;
+          }))
+        })
+
+        const result = await Promise.all(promises);
+        return result;
+      }
+
+      return curatedEvents;
+    });
+  }
 
   @httpGet("/:id")
   public async get(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
@@ -29,8 +50,28 @@ export class CuratedEventController extends ContentBaseController {
         const promises: Promise<CuratedEvent>[] = [];
         req.body.forEach(curatedEvent => {
           curatedEvent.churchId = au.churchId;
-          promises.push(this.repositories.curatedEvent.save(curatedEvent));
-        });
+          const saveFunction = async () => {
+            if (curatedEvent?.eventId) {
+              // If eventId is there, it's already pointed to a group event - save it directly.
+              return await this.repositories.curatedEvent.save(curatedEvent);
+            } else {
+              // If eventId is not there, it means the whole group needs to be added to the curated calendar. All the group events will be added to the curated calendar.
+              const groupEvents = await this.repositories.event.loadPublicForGroup(curatedEvent.churchId, curatedEvent.groupId);
+              if (groupEvents?.length > 0) {
+                //If events are there in a group, then save each event with it's ID in curated events.
+                for (const event of groupEvents) {
+                  return this.repositories.curatedEvent.save({...curatedEvent, eventId: event.id});
+                }
+              } else {
+                //If there are no events in a group, still allow them to add a group with eventId as NULL.
+                return await this.repositories.curatedEvent.save(curatedEvent)
+              }
+            }
+          }
+
+          promises.push(saveFunction());
+        })
+
         const result = await Promise.all(promises);
         return result;
       }
--- src/models/CuratedEvent.ts
+++ src/models/CuratedEvent.ts
@@ -1,7 +1,11 @@
+import { Event } from "./Event";
+
 export class CuratedEvent {
   public id?: string;
   public churchId?: string;
   public curatedCalendarId?: string;
   public groupId?: string;
   public eventId?: string;
+
+  public eventData?: Event;
 }
