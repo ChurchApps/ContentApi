d6914f90016c272665d2b0a33c114673346514a1
Jeremy Zongker <jeremy@zongker.net>
2023-09-25 14:26:24 -0500

    Added error handling for first file upload

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -51,7 +51,7 @@ export class FileController extends ContentBaseController {
       if (!au.checkAccess(Permissions.content.edit)) return this.json({}, 401);
       else {
         const totalBytes = await this.repositories.file.loadTotalBytes(au.churchId);
-        if (totalBytes.size > 100000000) return this.json({}, 401);
+        if (totalBytes?.size > 100000000) return this.json({}, 401);
         else {
           const key = "/" + au.churchId + "/files/" + req.body.fileName;
           const result = (Environment.fileStore === "S3") ? await AwsHelper.S3PresignedUrl(key) : {};
