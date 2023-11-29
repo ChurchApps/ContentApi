7da4dab3b4ed35b082c66ff5ec66b9647477da58
Himali Malvawala <himalimalvawala@gmail.com>
2023-11-29 16:40:47 +0530

    add: delete route for gallery images

--- src/controllers/GalleryController.ts
+++ src/controllers/GalleryController.ts
@@ -1,4 +1,4 @@
-import { controller, httpGet, httpPost, interfaces, requestParam } from "inversify-express-utils";
+import { controller, httpGet, httpPost, httpDelete, interfaces, requestParam } from "inversify-express-utils";
 import express from "express";
 import { ContentBaseController } from "./ContentBaseController"
 import { AwsHelper, FileStorageHelper } from "@churchapps/apihelper";
@@ -34,6 +34,16 @@ export class GalleryController extends ContentBaseController {
         const key = au.churchId + "/gallery/" + req.body.folder + "/" + req.body.fileName;
         const result = (Environment.fileStore === "S3") ? await AwsHelper.S3PresignedUrl(key) : {};
         return result;
+      }
+    });
+  }
+
+  @httpDelete("/:folder/:image")
+  public async delete(@requestParam("folder") folder: string, @requestParam("image") image: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.content.edit)) return this.json({}, 401);
+      else {
+        await FileStorageHelper.remove(au.churchId + "/gallery/" + folder + "/" + image);
       }
     });
   }
