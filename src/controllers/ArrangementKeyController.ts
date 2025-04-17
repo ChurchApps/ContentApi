795430b324a8ae8db221abba6010428d8578da72
Jeremy Zongker <jeremy@zongker.net>
2025-04-17 18:06:26 -0500

    Added presenter route for song details

--- src/controllers/ArrangementKeyController.ts
+++ src/controllers/ArrangementKeyController.ts
@@ -1,6 +1,6 @@
 import { controller, httpPost, httpGet, interfaces, requestParam, httpDelete } from "inversify-express-utils";
 import express from "express";
-import { ArrangementKey } from "../models";
+import { Arrangement, ArrangementKey, Song, SongDetail } from "../models";
 import { ContentBaseController } from "./ContentBaseController";
 import { Permissions } from "../helpers";
 
@@ -8,6 +8,19 @@ import { Permissions } from "../helpers";
 
 @controller("/arrangementKeys")
 export class ArrangementKeyController extends ContentBaseController {
+
+  @httpGet("/presenter/:churchId/:id")
+  public async getForPresenter(@requestParam("churchId") churchId: string, @requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<any> {
+    return this.actionWrapperAnon(req, res, async () => {
+      console.log(churchId, id);
+      const arrangementKey: ArrangementKey = await this.repositories.arrangementKey.load(churchId, id);
+      const arrangement: Arrangement = await this.repositories.arrangement.load(churchId, arrangementKey.arrangementId);
+      const song: Song = await this.repositories.song.load(churchId, arrangement.songId);
+      const songDetail: SongDetail = await this.repositories.songDetail.load(arrangement.songDetailId);
+      const result = { arrangementKey, arrangement, song, songDetail };
+      return result;
+    });
+  }
 
   @httpGet("/:id")
   public async get(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<any> {
